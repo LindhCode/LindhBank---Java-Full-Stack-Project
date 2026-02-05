d8e0bb1ef1e0cf09c3b31196aec340b28338826c
axellindh2004@gmail.com <axellindh2004@gmail.com>
2026-02-05 19:16:04 +0100

    Skapat möjligheten att registrera konton i en lokal ArrayList

--- src/main/java/com/example/bank/Account.java
+++ src/main/java/com/example/bank/Account.java
@@ -1,7 +1,10 @@
 package com.example.bank;
 
+import java.util.ArrayList;
+
 public class Account {
     private static int accNumb = 1111;
+    private static ArrayList<Account> accounts = new ArrayList<>();
 
     String username;
     String password;
@@ -15,7 +18,33 @@ public class Account {
         this.balance = balance;
     }
 
+    public void createNewAccount(String username, String password){
+
+        accounts.add(new Account(username, password,getAccNumb(),1000.00));
+        accNumb++;
+        for(Account a: accounts){
+            System.out.println(a);
+        }
+        //System.out.println(accounts.get(0));
+    }
+
     public static int getAccNumb() {
         return accNumb;
     }
+
+    public static ArrayList<Account> getAccounts(){
+        return accounts;
+    }
+
+    @Override
+    public String toString() {
+        return "Account{" +
+                "username='" + username  +
+                ", password=" + password +
+                ", accountNumber=" + accountNumber +
+                ", balance=" + balance +
+                '}';
+    }
+
+
 }
--- src/main/java/com/example/bank/CreateAccount.java
+++ src/main/java/com/example/bank/CreateAccount.java
@@ -1,10 +1,29 @@
 package com.example.bank;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class CreateAccount {
+    Account admin = new Account("admin","admin",0,0);
+
+    @FXML
+    private Button createAccount;
+    @FXML
+    private TextField username;
+    @FXML
+    private PasswordField password;
+
+
+    public void createAccountButton(){
+        if(!username.getText().isEmpty() || !password.getText().isEmpty()){
+            admin.createNewAccount(username.getText(),password.getText());
+        }
+    }
+
 
     // TODO - Skapa logik som skapar konto baserat på inparametrarna
     // i Account-klassen
